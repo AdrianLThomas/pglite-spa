ec364e57be139271d2f9e3bc29fb00170cb40b38
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2025-03-07 16:02:16 +0000

    feat: wired up TODOs roughly

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { PGlite } from "@electric-sql/pglite";
 import { drizzle } from "drizzle-orm/pglite";
 import { migrate } from "./app/db/migrate";
 import { todos } from "./app/db/schema";
+import { addTodoAction, deleteTodoAction } from "./app/db/actions";
 
 const client = new PGlite("idb://my-pgdata");
 const db = drizzle({ client });
@@ -15,14 +16,20 @@ const db = drizzle({ client });
 function App() {
   const [todoList, setTodoList] = useState<(typeof todos.$inferSelect)[]>([]);
 
-  const handleQuery = async () => {
-    let todoList = await db.select().from(todos).orderBy(todos.createdAt);
-    setTodoList(todoList)
-  };
-
   useEffect(() => {
     migrate();
   })
+
+  const handleAddTodoAction = async (formData: FormData) => {
+    const newTodo = await addTodoAction(formData);
+    setTodoList([...todoList, newTodo]);
+  };
+
+  const handleDeleteTodoAction = async (formData: FormData) => {
+    const deleted = await deleteTodoAction(formData);
+    setTodoList(todoList.filter((x) => x.id !== deleted.id));
+  };
+
 
   return (
     <>
@@ -39,13 +46,23 @@ function App() {
       </div>
       <h1>Vite + React + PGlite</h1>
 
-      <button onClick={handleQuery}>Let's query!</button>
+      <form action={handleAddTodoAction}>
+        <input type="text" name="content" required />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>
+            <span style={{ marginRight: "10px" }}>{todo.content}</span>
+            <form action={handleDeleteTodoAction} style={{ display: "inline" }}>
+              <input type="hidden" value={todo.id} name="id" />
+              <button type="submit">Delete</button>
+            </form>
+          </li>
+        ))}
+      </ul>
 
-      <div className="card">
-        <pre>{JSON.stringify(todoList)}</pre>
-      </div>
-
-      <p>REPL - TODO</p>
+      {/* <p>REPL - TODO</p> */}
     </>
   );
 }
---
+++ src/app/db/actions.ts
@@ -0,0 +1,19 @@
+import { db } from "./drizzle";
+import { todos } from "./schema";
+import { eq } from "drizzle-orm";
+
+export async function addTodoAction(formData: FormData) {
+  const todo: typeof todos.$inferInsert = {
+    content: formData.get("content") as string,
+  };
+  const inserted = await db.insert(todos).values(todo).returning();
+
+  return inserted[0];
+}
+
+export async function deleteTodoAction(formData: FormData) {
+  const id = parseInt(formData.get("id") as string);
+  const deleted = await db.delete(todos).where(eq(todos.id, id)).returning();
+
+  return deleted[0];
+}
