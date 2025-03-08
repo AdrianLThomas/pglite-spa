a6b807b6be60639df3bf0aecdc9f38031127e520
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2025-03-08 08:44:25 +0000

    refactor: move todos in to own component and use suspense fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
-import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import pgliteLogo from "./assets/pglite.svg";
 import "./App.css";
 
-import { migrate } from "./app/db/migrate";
-import { todos } from "./app/db/schema";
-import { addTodoAction, deleteTodoAction, fetchAllTodos } from "./app/db/actions";
+import Todos from "./components/Todos";
+import { Suspense } from "react";
 
 function App() {
-  const [todoList, setTodoList] = useState<(typeof todos.$inferSelect)[]>([]);
-
-  useEffect(() => {
-    const setup = async () => {
-      await migrate();
-
-      const todoList = await fetchAllTodos();
-      setTodoList(todoList);
-    }
-
-    setup();
-  })
-
-  const handleAddTodoAction = async (formData: FormData) => {
-    const newTodo = await addTodoAction(formData);
-    setTodoList([...todoList, newTodo]);
-  };
-
-  const handleDeleteTodoAction = async (formData: FormData) => {
-    const deleted = await deleteTodoAction(formData);
-    setTodoList(todoList.filter((x) => x.id !== deleted.id));
-  };
-
-
   return (
     <>
       <div>
@@ -47,24 +21,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + PGlite</h1>
-
-      <form action={handleAddTodoAction}>
-        <input type="text" name="content" required />
-        <button type="submit">Add Todo</button>
-      </form>
-      <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>
-            <span style={{ marginRight: "10px" }}>{todo.content}</span>
-            <form action={handleDeleteTodoAction} style={{ display: "inline" }}>
-              <input type="hidden" value={todo.id} name="id" />
-              <button type="submit">Delete</button>
-            </form>
-          </li>
-        ))}
-      </ul>
-
-      {/* <p>REPL - TODO</p> */}
+      
+      <Suspense fallback={<p>Loading database...</p>}>
+        <Todos/>
+      </Suspense>
     </>
   );
 }
---
+++ src/components/Todos.tsx
@@ -0,0 +1,47 @@
+import {  use, useState } from "react";
+
+import { migrate } from "../app/db/migrate";
+import { todos } from "../app/db/schema";
+import { addTodoAction, deleteTodoAction, fetchAllTodos } from "../app/db/actions";
+
+const setup = (async () => {
+  await migrate();
+
+  return await fetchAllTodos();
+})()
+
+export default function Todos() {
+  const [todoList, setTodoList] = useState<(typeof todos.$inferSelect)[]>(use(setup));
+
+  const handleAddTodoAction = async (formData: FormData) => {
+    const newTodo = await addTodoAction(formData);
+    setTodoList([...todoList, newTodo]);
+  };
+
+  const handleDeleteTodoAction = async (formData: FormData) => {
+    const deleted = await deleteTodoAction(formData);
+    setTodoList(todoList.filter((x) => x.id !== deleted.id));
+  };
+
+  return (
+    <>
+      <form action={handleAddTodoAction}>
+        <input type="text" name="content" required />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>
+            <span style={{ marginRight: "10px" }}>{todo.content}</span>
+            <form action={handleDeleteTodoAction} style={{ display: "inline" }}>
+              <input type="hidden" value={todo.id} name="id" />
+              <button type="submit">Delete</button>
+            </form>
+          </li>
+        ))}
+      </ul>
+
+      {/* <p>REPL - TODO</p> */}
+    </>
+  );
+}
