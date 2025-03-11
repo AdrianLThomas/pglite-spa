69ee186b4053f71aff6e35ab64c31423c26ce061
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2025-03-11 13:31:17 +0000

    refactor: dead config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,4 @@ export default {
   out: "./src/app/db/migrations",
   dialect: "postgresql",
   driver: "pglite",
-//   dbCredentials: {
-//     url: process.env.DATABASE_URL_EXTERNAL!,
-//   },
 } satisfies Config;
