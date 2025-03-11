3636c8d96173c4b6728dd1c9135398dcd16d5bc7
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2025-03-11 13:06:41 +0000

    fix: vite config deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pglite-spa', // running under this path for demo purposes
   plugins: [react()],
   optimizeDeps: {
     exclude: ["@electric-sql/pglite"],
